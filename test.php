f1d797a06f9870731d0858caa4026dba5d2d4aa5
Your Name <you@example.com>
2025-07-16 16:53:47 +0300

    узнать id значений

--- test.php
+++ test.php
@@ -1,19 +1,17 @@
 <?php
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 use Bitrix\Main\Loader;
-
 Loader::includeModule('crm');
 
+$ufFieldName = 'UF_CRM_1752661461592'; // ← сюда подставь имя поля, которое нашёл
+
+$res = CUserFieldEnum::GetList([], ['USER_FIELD_NAME' => $ufFieldName]);
+
 echo "<pre>";
-
-$res = CUserTypeEntity::GetList([], ['ENTITY_ID' => 'CRM_DEAL']);
-
-while ($field = $res->Fetch()) {
-    echo "FIELD_NAME: " . $field['FIELD_NAME'] . "\n";
-    echo "LABEL: " . $field['EDIT_FORM_LABEL'] . "\n";
-    echo "TYPE: " . $field['USER_TYPE_ID'] . "\n";
-    echo "--------------------------\n";
+while ($item = $res->Fetch()) {
+    echo "ID: " . $item['ID'] . "\n";
+    echo "VALUE: " . $item['VALUE'] . "\n";
+    echo "-------------------\n";
 }
-
 echo "</pre>";
