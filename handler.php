2321e920810a837d86f53eb446b5fdf01d75b552
Your Name <you@example.com>
2025-07-16 13:48:20 +0300

    проверка веб хука

---
+++ handler.php
@@ -0,0 +1,11 @@
+<?php
+
+    $method = 'crm.deal.userfield.list'; // Для других сущностей замените `deal` (например, `lead`, `contact`)
+    $webhookUrl = 'http://192.168.1.73/rest/1/aod0scfba11vm0y9/';
+
+    $response = file_get_contents($webhookUrl . $method . '.json');
+    $data = json_decode($response, true);
+
+    print_r($data['result']);
+
+    exit;
